0a9bccf34cfe299d5c12503eaa269bbc9b56e0ac
Ferrando Carlos <cferrando2015@gmail.com>
2026-04-15 23:43:48 -0300

    feat(views): configurar layout base y configuración en app

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.set('views', path.join(__dirname, 'src', 'views'));
 // Ruta de prueba inicial
 app.get('/', (req, res) => {
     res.render('index', {
-        title: 'Control de Gastos'
+        title: 'Inicio | Control de Gastos'
     });
 });
 
